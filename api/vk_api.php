ad4e1e5f76515e2fa592ba84ad1eaef9c4bca47c
Dmitriy <boyarin.dmitriy1998@gmail.com>
2018-06-02 19:15:46 +0300

    OK

--- api/vk_api.php
+++ api/vk_api.php
@@ -10,26 +10,6 @@ function vkApi_messagesSend($peer_id, $message) {
     ));
 }
 
-function vkApi_usersGet($user_id) {
-    return _vkApi_call('users.get', array(
-        'user_id' => $user_id,
-    ));
-}
-
-function vkApi_docsGetMessagesUploadServer($peer_id, $type) {
-    return _vkApi_call('docs.getMessagesUploadServer', array(
-        'peer_id' => $peer_id,
-        'type' => $type,
-    ));
-}
-
-function vkApi_docsSave($file, $title) {
-    return _vkApi_call('docs.save', array(
-        'file' => $file,
-        'title' => $title,
-    ));
-}
-
 function _vkApi_call($method, $params = array()) {
   $params['access_token'] = VK_API_ACCESS_TOKEN;
   $params['v'] = VK_API_VERSION;
@@ -55,27 +35,3 @@ function _vkApi_call($method, $params = array()) {
   return $response['response'];
 }
 
-function vkApi_upload($url, $file_name) {
-  if (!file_exists($file_name)) {
-      print('File is note exist');
-  }
-
-  $curl = curl_init($url);
-  curl_setopt($curl, CURLOPT_POST, true);
-  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($curl, CURLOPT_POSTFIELDS, array('file' => new CURLfile($file_name)));
-  $json = curl_exec($curl);
-  $error = curl_error($curl);
-  if ($error) {
-      print('vkApi_upload Error');
-  }
-
-  curl_close($curl);
-
-  $response = json_decode($json, true);
-  if (!$response) {
-    print('Invalid response');
-  }
-
-  return $response;
-}
